7c379b4c657654688faae6c362b615a772969a06
Aman Saifi <amansaifi0019@gmail.com>
2025-08-02 14:18:00 +0530

    Al files are added

---
+++ calc.py
@@ -0,0 +1,18 @@
+# Calculator
+
+operator = input("Enter operator (+, -, *, /): ")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+if operator == "+":
+    result = num1 + num2
+elif operator == "-":
+    result = num1 - num2
+elif operator == "*":
+    result = num1 * num2
+elif operator == "/":
+    result = num1 / num2
+else:
+    result = "Invalid operator"
+
+print("Result:", result)
---
+++ first.py
@@ -0,0 +1,11 @@
+a = 10
+b = 20
+
+# if (a > b):
+#     max_num = a
+# else:
+#     max_num = b
+
+
+max_num = a if a > b else b
+print(max_num)
